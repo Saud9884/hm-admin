bc079e3eaa09a81de809c32c1e78c6a0e3ef2719
Saud9884 <saudlohar85@gmail.com>
2023-10-03 14:53:02 +0530

    Added Navigation

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import Nav from "@/components/Nav";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Layout({children}) {
+  const {data: session} = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-400 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="bg-black min-h-screen flex">
+      <Nav />
+      <div className="flex-grow bg-white mt-2 mr-2 rounded-lg">
+        {children}
+      </div>
+      </div>
+    
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,9 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const {data: session} = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-400 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div className="flex justify-end">
-      <div className="flex bg-blue-400 gap-1 text-white rounded-lg overflow-hidden">
-        <img src={session?.user?.image} alt="" className="w-6 h-6"/>
-        <span className="px-2">
-          {session?.user?.name}
-        </span>
-      </div>
-      </div>
-    
-  )
+return(
+  <Layout>
+    Test
+  </Layout>
+)
 }
