38cc3d576790973a6a6446a48cc56a0569bb293c
Saud9884 <saudlohar85@gmail.com>
2023-11-23 14:57:00 +0530

    Added parent category option into the category page

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -7,14 +7,15 @@ export default async function handle(req, res) {
     await mongooseConnect();
 
     if(method === 'GET'){
-        res.json(await Category.find());
+        res.json(await Category.find().populate('parent'));
     }
 
     if(method === 'POST'){
-        const {name} = req.body;
+        const {name,parentCategory} = req.body;
 
         const categoryDoc = await Category.create({
-            name
+            name,
+            parent:parentCategory
            });
        
           res.json(categoryDoc);
--- pages/categories.js
+++ pages/categories.js
@@ -5,40 +5,60 @@ import { useEffect, useState } from "react";
 export default function Categories() {
     const [name, setName] = useState('');
     const [categories, setCategories] = useState([]);
+    const [parentCategory, setParentCategory] = useState('');
 
     useEffect(() => {
-        axios
-          .get("/api/categories")
-          .then(res => setCategories(res.data))
-          .catch(err => console.error(err));
+       fetchCategories();
     }, []);
+
+    function fetchCategories() {
+        axios
+        .get("/api/categories")
+        .then(res => setCategories(res.data))
+        .catch(err => console.error(err));
+    }
 
     async function saveCategory(e) {
         e.preventDefault();
-        await axios.post('/api/categories', {name});
+        await axios.post('/api/categories', {name,parentCategory});
         setName('');
+        fetchCategories();
     }
 
     return(
        <Layout>
         <h1>Categories</h1>
+
         <div className="mt-4">
         <label>Add New Category</label>
-        <form onSubmit={saveCategory} className="flex gap-5">
+        <form onSubmit={saveCategory} className="flex gap-4">
         <input className="mb-0" type="text" placeholder="Enter Name of the Category" value={name} onChange={e => setName(e.target.value)}/>
+
+        <select className="mb-0"
+            value={parentCategory}
+            onChange={e => setParentCategory(e.target.value)}>
+            <option value="">No Parent category</option>
+            {categories.length > 0 && categories.map(category => (
+                    <option value={category._id}>{category.name}</option>
+                ))}
+        </select>
+
         <button type="submit" className="btn-black">Save</button>
         </form>
         </div>
+        
         <table className="mt-8">
             <thead>
                 <tr>
                     <td>Category Name</td>
+                    <td>Parent Category</td>
                 </tr>
             </thead>
             <tbody>
                 {categories.length > 0 && categories.map(category => (
                     <tr>
                         <td>{category.name}</td>
+                        <td>{category?.parent?.name}</td>
                     </tr>
                 ))}
             </tbody>
