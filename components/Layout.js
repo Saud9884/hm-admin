459a63edb45755ca3b92dd59de5be21efe11bc6f
Saud9884 <saudlohar85@gmail.com>
2023-11-29 16:27:39 +0530

    Made Navigation responsive

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,56 @@
 import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import Logo from "./Logo";
 
-export default function Layout({children}) {
-  const {data: session} = useSession();
-   if (!session) {
+export default function Layout({ children }) {
+  const [showNav, setShowNav] = useState(false);
+  const { data: session } = useSession();
+  if (!session) {
     return (
       <div className="bg-blue-400 w-screen h-screen flex items-center">
         <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+          <button
+            onClick={() => signIn("google")}
+            className="bg-white p-2 px-4 rounded-lg"
+          >
+            Login with Google
+          </button>
         </div>
       </div>
     );
   }
   return (
-    <div className="bg-black min-h-screen flex">
-      <Nav />
-      <div className="flex-grow bg-white mt-2 mr-2 rounded-lg p-4">
-        {children}
+    <div className="bg-black min-h-screen">
+      <div className="block md:hidden flex items-center p-4">
+        <button onClick={() => setShowNav(true)} className="text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className="text-white flex grow justify-center mr-6">
+        <Logo />
+        </div>
+        
       </div>
+
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="flex-grow bg-white mt-2 mr-2 rounded-lg p-4">
+          {children}
+        </div>
       </div>
-    
-  )
+    </div>
+  );
 }
