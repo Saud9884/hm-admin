2ae171020c030ca6339a308f2f93502c208be6ca
Saud9884 <saudlohar85@gmail.com>
2023-12-12 17:50:26 +0530

    Create order pages and listed orders

--- models/Product.js
+++ models/Product.js
@@ -11,6 +11,8 @@ const ProductSchema = new Schema({
   quantity: { type: Number, required: true },
   category: { type: mongoose.Types.ObjectId, ref: "Category" },
   properties: { type: Object },
+}, {
+  timestamps: true,
 });
 
 export const Product = models?.Product || model("Product", ProductSchema);
---
+++ pages/orders.js
@@ -0,0 +1,48 @@
+import Layout from "@/components/Layout";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function OrdersPage() {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/orders")
+      .then((res) => setOrders(res.data))
+      .catch((err) => console.error(err));
+  });
+  return (
+    <Layout>
+      <h1>Orders</h1>
+      <table className="basic">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Recipient</th>
+            <th>Produts</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.length > 0 &&
+            orders.map((order) => (
+              <tr>
+                <td>{new Date(order.createdAt).toLocaleString()}</td>
+                <td>
+                  {order.name} {order.email} {order.phoneNumber}{" "}
+                  {order.streetAddress} {order.city} {order.postalCode}{" "}
+                  {order.country}{" "}
+                </td>
+                <td>
+                    {order.line_items.map(l => (
+                        <>
+                        {l.price_data?.product_data.name} x {l.quantity}<br />
+                        
+                        </>
+                    ))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+}
